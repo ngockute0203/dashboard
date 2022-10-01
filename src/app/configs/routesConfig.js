b78496fd2690954388e5434cd3868947415d4b15
peanh <riddnicky0203@gmail.com>
2022-10-01 23:19:03 +0700

    ngoc 2

--- src/app/configs/routesConfig.js
+++ src/app/configs/routesConfig.js
@@ -2,19 +2,33 @@ import FuseUtils from '@fuse/utils';
 import FuseLoading from '@fuse/core/FuseLoading';
 import { Navigate } from 'react-router-dom';
 import settingsConfig from 'app/configs/settingsConfig';
+import userInterfaceConfigs from '../main/user-interface/UserInterfaceConfigs';
 import SignInConfig from '../main/sign-in/SignInConfig';
 import SignUpConfig from '../main/sign-up/SignUpConfig';
 import SignOutConfig from '../main/sign-out/SignOutConfig';
-import Error404Page from '../main/404/Error404Page';
-import ExampleConfig from '../main/example/ExampleConfig';
+import dashboardsConfigs from '../main/dashboards/dashboardsConfigs';
+import appsConfigs from '../main/apps/appsConfigs';
+import pagesConfigs from '../main/pages/pagesConfigs';
+import authRoleExamplesConfigs from '../main/auth/authRoleExamplesConfigs';
+import DocumentationConfig from '../main/documentation/DocumentationConfig';
 
-const routeConfigs = [ExampleConfig, SignOutConfig, SignInConfig, SignUpConfig];
+const routeConfigs = [
+  ...appsConfigs,
+  ...dashboardsConfigs,
+  ...pagesConfigs,
+  ...authRoleExamplesConfigs,
+  ...userInterfaceConfigs,
+  DocumentationConfig,
+  SignOutConfig,
+  SignInConfig,
+  SignUpConfig,
+];
 
 const routes = [
   ...FuseUtils.generateRoutesFromConfigs(routeConfigs, settingsConfig.defaultAuth),
   {
     path: '/',
-    element: <Navigate to="/example" />,
+    element: <Navigate to="dashboards/analytics" />,
     auth: settingsConfig.defaultAuth,
   },
   {
@@ -22,12 +36,8 @@ const routes = [
     element: <FuseLoading />,
   },
   {
-    path: '404',
-    element: <Error404Page />,
-  },
-  {
     path: '*',
-    element: <Navigate to="404" />,
+    element: <Navigate to="pages/error/404" />,
   },
 ];
 
